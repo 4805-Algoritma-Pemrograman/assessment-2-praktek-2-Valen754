dfabf89635e4513a27bb4c6f14021a050cb3a6d2
Valen754 <hllzrd@gmail.com>
2024-12-12 10:41:31 +0700

    a

--- Nomor2.java
+++ Nomor2.java
@@ -23,11 +23,11 @@ public class Nomor2 {
                 break;
             }
         }
+        String a = input.nextLine();
 
         int totalHarga = hitungTotalHarga(hargaBelanja);
         int voucherBelanja = hitungVoucherBelanja(totalHarga);
 
-        String a = input.nextLine();
 
         System.out.println("Total voucher belanja : " + voucherBelanja);
         System.out.println("Total yang harus dibayar : " + totalHarga);
